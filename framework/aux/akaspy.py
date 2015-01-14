21ee49374b46f14713fcf116dd5010a91cb6b0b7
rotlogix <rotlogix@gmail.com>
2015-01-14 17:04:32 -0500

    Added logging to akaspy module

--- framework/aux/akaspy.py
+++ framework/aux/akaspy.py
@@ -1,80 +1,81 @@
 import dns.resolver
 import socket
 import errno
+
 from blessings import Terminal
-from framework.aux.auxiliary_enums import AuxiliaryEnums
 t = Terminal()
 
+from framework.aux.auxiliary_enums import AuxiliaryEnums
+from framework.utils.logger import logger
 
-class akaspy_command(object):
+
+class AkaspyAction(object):
 
     def __init__(self, domain, target):
+
         self.domain = domain
         self.target = target
 
     def do_bypass(self, ip):
 
-        # Setup socket parameters
+        """ Bypass Akamai
+        """
+
+        # Set some shit up first
         port = AuxiliaryEnums.HTTP_PORT
         address = (ip, port.value)
         url = self.target
         request = "GET / HTTP/1.1\r\nHost: {0}\r\n\r\n".format(url)
 
         try:
-            # Create socket connection and HTTP request
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             s.connect(address)
             s.send(request.encode())
             data = s.recv(15)
-            code = "200"
-
-            # Check HTTP response code
-            if code in data.decode('utf-8'):
-                print(t.green(">>> ") + "Origin Access Possible!")
+            if "200" in data.decode('utf-8'):
+                logger("info", "Origin Access Possible!")
             else:
-                print(t.red(">>> ") + "Origin Access Not Possible")
-                print(t.red(">>> ") + "HTTP Status Code :: {0}".format(data.decode('utf-8')))
+                logger("warning", "Origin Access Not Possible! :: {0}".format(t.yellow(data.decode("utf-8"))))
 
+            # Don't forget to close the socket
             s.close()
 
         except socket.error as e:
-
+            # Handle some exceptions
             if e.errno != errno.ECONNREFUSED:
-                print(t.red(">>> ") + "Connection Issue!")
+                logger("warning", "Connection Issue!")
                 raise e
             else:
-                print(t.red(">>> ") + "Socket Refused!")
+                logger("warning", "Socket Refused!")
                 pass
 
     def check_origin(self):
 
+        """ Check the origin
+        """
+
         try:
-
+            # Set some shit up first
             answer = dns.resolver.query(self.domain)
-
-            # Check for DNS record
             if answer.qname == answer.canonical_name:
-                print(t.red(">>> ") + "Incorrect DNS Record :: {0}".format(answer.qname))
-                print(t.red(">>> ") + "Origin Not Accessible")
+                logger("warning", "Incorrect DNS Record :: {0}".format(t.yellow(str(answer.qname))))
+                logger("info", "Origin Not Accessible!")
             else:
-                print(t.green(">>> ") + "Retrieved an alias! :: {0}".format(answer.canonical_name))
+                logger("info", "Retrieved An Alias! :: {0}".format(t.yellow(str(answer.canonical_name))))
 
             response = dns.resolver.query(answer.canonical_name, 'A')
 
             for response_data in response:
                 ip = response_data.address
                 if ip:
-                    print(t.green(">>> ") + "Getting IP Address :: {0}".format(ip))
-
-                    # Try the origin bypass
+                    logger("info", "Getting IP Address :: {0}".format(t.yellow(ip)))
                     self.do_bypass(ip)
-
         except dns.resolver.NXDOMAIN:
-            print(t.red(">>> ") + "Domain Not Found!")
+            logger("warning", "Domain Not Found!")
             pass
         except dns.resolver.Timeout:
-            print(t.red(">>> ") + "Query Timed Out!")
+            logger("warning", "Query Timed Out!")
             pass
         except dns.exception.DNSException:
-            print(t.red(">>> ") + "DNS Exception!")
+            logger("warning", "DNS Exception!")
             pass
